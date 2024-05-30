e56761b7d7fd6b5379bb84588a43865ae8a56f72
Wambuku <julietwambuku361@gmail.com>
2024-05-30 18:25:34 +0300

    Customer Model

---
+++ Backend/app/Http/Controllers/CustomerController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        $customers = Customer::all();
+        return response()->json($customers);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:customers,email',
+            'phone' => 'required',
+            'address' => 'required',
+        ]);
+
+        return Customer::create($request->all());
+    }
+
+    public function show($id)
+    {
+        $customer = Customer::findOrFail($id);
+        return response()->json($customer);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:customers,email,'.$id,
+            'phone' => 'required',
+            'address' => 'required',
+        ]);
+
+        $customer = Customer::findOrFail($id);
+        $customer->update($request->all());
+
+        return $customer;
+    }
+
+    public function destroy($id)
+    {
+        $customer = Customer::findOrFail($id);
+        $customer->delete();
+
+        return response()->json(null, 204);
+    }
+}
+
